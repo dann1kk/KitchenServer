a2e722c882dbf47dba07640613fe2ebd24683b06
dann1kk <daniel.pogorevici@isa.utm.md>
2022-09-20 19:22:44 +0300

    restructured and added separate files for imports

---
+++ imports/Cooks.py
@@ -0,0 +1,28 @@
+global Cooks
+
+
+Cooks = [{
+    "id": 1,
+    "rank": 3,
+    "proficiency": 4,
+    "name": "Jamie Oliver",
+    "catch-phrase": "Can I put some olive oil in your food?"
+}, {
+    "id": 2,
+    "rank": 2,
+    "proficiency": 3,
+    "name": "Gordon Ramsay",
+    "catch-phrase": "You f****** donkey"
+}, {
+    "id": 3,
+    "rank": 2,
+    "proficiency": 2,
+    "name": "Nusret Gökçe",
+    "catch-phrase": "Salt Bae"
+}, {
+    "id": 4,
+    "rank": 1,
+    "proficiency": 2,
+    "name": "Guy Fieri",
+    "catch-phrase": "Nice sauce man"
+}]
